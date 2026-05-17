5fc4d9a9bb7f3b2e7620298b0c21361b90ae040e
Miguel Nascimento <miguel.nasto@gmail.com>
2026-05-17 16:16:02 -0300

    refactor: add support for Controller extension and abstract class

--- api/src/main/java/com/estaciona/controller/MarcaController.java
+++ api/src/main/java/com/estaciona/controller/MarcaController.java
@@ -1,45 +1,45 @@
 package com.estaciona.controller;
 
-import com.estaciona.model.IService;
 import com.estaciona.model.Marca;
 import com.estaciona.service.JpaMarcaService;
 import java.util.List;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/marcas")
-public class MarcaController {
-  private final IService<Marca, Short> service;
-
+public class MarcaController extends SupportController<Marca, Short> {
   public MarcaController(JpaMarcaService service) {
-    this.service = service;
+    super(service);
   }
 
   @GetMapping
   public ResponseEntity<List<Marca>> all() {
-    return ResponseEntity.ok(service.getAll());
+    return super.all();
   }
 
   @GetMapping("/{id}")
   public ResponseEntity<Marca> find(@PathVariable Short id) {
-    return service.findById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    return super.find(id);
   }
 
   @PostMapping
   public ResponseEntity<Marca> create(@RequestBody Marca data) {
-    return ResponseEntity.status(HttpStatus.CREATED).body(service.save(data));
+    return super.create(data);
   }
 
   @PutMapping("/{id}")
   public ResponseEntity<Marca> update(@PathVariable Short id, @RequestBody Marca data) {
-    data.setId(id);
-    return service.update(data).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    return super.update(id, data);
   }
 
   @DeleteMapping("/{id}")
   public ResponseEntity<Void> delete(@PathVariable Short id) {
-    return (service.delete(id) ? ResponseEntity.noContent() : ResponseEntity.notFound()).build();
+    return super.delete(id);
+  }
+
+  @Override
+  protected String getURL() {
+    return "/marcas";
   }
 }
