7ec25c23d18bde602efb62ae300051026ffa5406
Miguel Nascimento <miguel.nasto@gmail.com>
2026-05-17 12:35:36 -0300

    refactor: add delete REST endpoint

--- api/src/main/java/com/estaciona/controller/MarcaController.java
+++ api/src/main/java/com/estaciona/controller/MarcaController.java
@@ -1,7 +1,8 @@
 package com.estaciona.controller;
 
+import com.estaciona.model.IService;
 import com.estaciona.model.Marca;
-import com.estaciona.service.MarcaService;
+import com.estaciona.service.JpaMarcaService;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,9 +11,9 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/marcas")
 public class MarcaController {
-  private final MarcaService service;
+  private final IService<Marca, Short> service;
 
-  public MarcaController(MarcaService service) {
+  public MarcaController(JpaMarcaService service) {
     this.service = service;
   }
 
@@ -33,14 +34,11 @@ public class MarcaController {
 
   @PutMapping("/{id}")
   public ResponseEntity<Marca> updateUser(@PathVariable Short id, @RequestBody Marca data) {
-    return service
-        .update(id, data.getNome())
-        .map(ResponseEntity::ok)
-        .orElse(ResponseEntity.notFound().build());
+    return service.update(data).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
   }
 
-  // @DeleteMapping("/marcas/{id}")
-  // public ResponseEntity<Void> deleteMarca(@PathVariable Short id) {
-  //   return (service.delete(id) ? ResponseEntity.noContent() : ResponseEntity.notFound()).build();
-  // }
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> deleteMarca(@PathVariable Short id) {
+    return (service.delete(id) ? ResponseEntity.noContent() : ResponseEntity.notFound()).build();
+  }
 }
