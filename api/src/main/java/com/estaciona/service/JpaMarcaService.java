ce6b01d4fcb184532175e6cd14df49377423eb2c
Miguel Nascimento <miguel.nasto@gmail.com>
2026-05-17 16:13:56 -0300

    refactor: update Marca Service for abstracct class

--- api/src/main/java/com/estaciona/service/JpaMarcaService.java
+++ api/src/main/java/com/estaciona/service/JpaMarcaService.java
@@ -2,17 +2,16 @@ package com.estaciona.service;
 
 import com.estaciona.model.Marca;
 import com.estaciona.repository.MarcaRepository;
-import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JpaMarcaService extends JpaService<Marca, Short> {
   public JpaMarcaService(MarcaRepository repo) {
-    super(repo);
+    super(repo, Marca.class);
   }
 
   @Override
-  public Optional<Marca> update(Marca newData) {
+  public Marca update(Marca newData) {
     return super.update(
         newData.getId(),
         m -> {
