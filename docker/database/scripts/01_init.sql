81f33b20fba953164e844329792bb450ace1ade1
Miguel Nascimento <miguel.nasto@gmail.com>
2026-05-18 20:26:20 -0300

    refactor(database): replace SERIAL with IDENTITY
    Replace SERIAL and SMALLSERIAL declarations with
    GENERATED ALWAYS AS IDENTITY equivalents.

--- docker/database/scripts/01_init.sql
+++ docker/database/scripts/01_init.sql
@@ -1,12 +1,30 @@
 CREATE TABLE marcas (
+<<<<<<< Updated upstream
    id SMALLSERIAL,
+<<<<<<< Updated upstream
    nome VARCHAR(32) UNIQUE NOT NULL,
+=======
+   nome VARCHAR(32),
+=======
+   id SMALLINT GENERATED ALWAYS AS IDENTITY,
+   nome VARCHAR(32) UNIQUE NOT NULL,
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
    PRIMARY KEY(id)
 );
 
 CREATE TABLE modelos (
+<<<<<<< Updated upstream
    id SMALLSERIAL,
+<<<<<<< Updated upstream
    nome VARCHAR(32) NOT NULL,
+=======
+   nome VARCHAR(32),
+=======
+   id SMALLINT GENERATED ALWAYS AS IDENTITY,
+   nome VARCHAR(32) NOT NULL,
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
    marca_id SMALLINT NOT NULL,
    PRIMARY KEY (id),
    CONSTRAINT uq_modelo_marca UNIQUE(nome, marca_id),
@@ -14,15 +32,24 @@ CREATE TABLE modelos (
 );
 
 CREATE TABLE clientes (
-   id SERIAL,
+   id INTEGER GENERATED ALWAYS AS IDENTITY,
    nome VARCHAR(150) NOT NULL,
    telefone VARCHAR(15) NOT NULL,
    PRIMARY KEY(id)
 );
 
 CREATE TABLE carros (
+<<<<<<< Updated upstream
    id SERIAL,
+<<<<<<< Updated upstream
    placa CHAR(7) UNIQUE NOT NULL,
+=======
+   placa VARCHAR(7) UNIQUE NOT NULL,
+=======
+   id INTEGER GENERATED ALWAYS AS IDENTITY,
+   placa CHAR(7) UNIQUE NOT NULL,
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
    cor SMALLINT NOT NULL,
    modelo_id SMALLINT NOT NULL,
    PRIMARY KEY(id),
@@ -38,9 +65,20 @@ CREATE TABLE responsaveis_carros (
 );
 
 CREATE TABLE precos (
+<<<<<<< Updated upstream
    id SMALLSERIAL,
+<<<<<<< Updated upstream
    preco DECIMAL(10, 2) NOT NULL CHECK(preco >= 0),
    nome VARCHAR(100) NOT NULL,
+=======
+   preco DECIMAL(10, 2),
+   nome VARCHAR(100),
+=======
+   id SMALLINT GENERATED ALWAYS AS IDENTITY,
+   preco DECIMAL(10, 2) NOT NULL CHECK(preco >= 0),
+   nome VARCHAR(100) NOT NULL,
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
    descricao TEXT,
    PRIMARY KEY(id)
 );
