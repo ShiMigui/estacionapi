8d281bab1709146156b133b4ead99b5653c140c1
Miguel Nascimento <miguel.nasto@gmail.com>
2026-05-18 22:34:15 -0300

    refactor: remove final stmt from setId

--- api/src/main/java/com/estaciona/model/AbstractEntity.java
+++ api/src/main/java/com/estaciona/model/AbstractEntity.java
@@ -8,15 +8,12 @@ public abstract class AbstractEntity<ID> {
 
   protected abstract void internalSetId(ID id);
 
-  public final void setId(ID id) {
+  public void setId(ID id) {
     ID current = getId();
-
     if (current != null) {
       if (current.equals(id)) return;
-
       throw new IllegalArgumentException("ID já foi definido e não pode ser alterado");
     }
-
     internalSetId(id);
   }
 }
