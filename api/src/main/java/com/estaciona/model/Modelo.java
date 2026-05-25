476a67b04bf401490c7aba34e698de14f5cfb784
Miguel Nascimento <miguel.nasto@gmail.com>
2026-05-25 09:30:18 -0300

    refactor: change marca loader from EAGER to LAZY

--- api/src/main/java/com/estaciona/model/Modelo.java
+++ api/src/main/java/com/estaciona/model/Modelo.java
@@ -32,7 +32,7 @@ public class Modelo extends AbstractEntity<Short> {
   @NotNull
   @JsonIgnoreProperties("modelos")
   @JoinColumn(name = "marca_id", nullable = false)
-  @ManyToOne(fetch = FetchType.EAGER, optional = false)
+  @ManyToOne(fetch = FetchType.LAZY, optional = false)
   private Marca marca;
 
   public void rename(String nome) {
