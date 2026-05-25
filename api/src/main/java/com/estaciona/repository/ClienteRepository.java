cc63db03acf604d9f395d591a0846a361d01dcfb
Miguel Nascimento <miguel.nasto@gmail.com>
2026-05-25 19:48:45 -0300

    feat: add Cliente repository

---
+++ api/src/main/java/com/estaciona/repository/ClienteRepository.java
@@ -0,0 +1,8 @@
+package com.estaciona.repository;
+
+import com.estaciona.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {}
