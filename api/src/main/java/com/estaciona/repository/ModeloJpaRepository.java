2997b6d02c5d4981fe7c677b65728b761fba6c71
Miguel Nascimento <miguel.nasto@gmail.com>
2026-05-25 09:31:18 -0300

    refactor: override findAll and findById, to execute a specific query

--- api/src/main/java/com/estaciona/repository/ModeloJpaRepository.java
+++ api/src/main/java/com/estaciona/repository/ModeloJpaRepository.java
@@ -1,8 +1,20 @@
 package com.estaciona.repository;
 
 import com.estaciona.model.Modelo;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ModeloJpaRepository extends JpaRepository<Modelo, Short> {}
+public interface ModeloJpaRepository extends JpaRepository<Modelo, Short> {
+  @Query("SELECT DISTINCT m FROM Modelo m LEFT JOIN FETCH m.marca")
+  @Override
+  List<Modelo> findAll();
+
+  @Query("SELECT DISTINCT m FROM Modelo m LEFT JOIN FETCH m.marca WHERE m.id = :id")
+  @Override
+  Optional<Modelo> findById(@Param("id") Short id);
+}
