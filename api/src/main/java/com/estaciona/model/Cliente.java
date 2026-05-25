1bfaac2e5e685343d5b4a7f9472471ac994201de
Miguel Nascimento <miguel.nasto@gmail.com>
2026-05-25 19:48:26 -0300

    feat: add Cliente entity

---
+++ api/src/main/java/com/estaciona/model/Cliente.java
@@ -0,0 +1,50 @@
+package com.estaciona.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "clientes")
+@ToString(onlyExplicitlyIncluded = true)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Cliente extends AbstractEntity<Long> {
+  @Id
+  @ToString.Include
+  @Column(nullable = false)
+  @EqualsAndHashCode.Include
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @NotBlank
+  @Size(max = 150)
+  @Column(nullable = false)
+  private String nome;
+
+  @NotBlank
+  @Size(max = 15)
+  @Column(nullable = false)
+  private String telefone;
+
+  public void setNome(String nome) {
+    if (nome == null || nome.isBlank())
+      throw new IllegalArgumentException("Nome não pode ser vazio");
+
+    this.nome = nome.trim();
+  }
+
+  public void setTelefone(String telefone) {
+    if (telefone == null || !telefone.matches("\\d{10,15}"))
+      throw new IllegalArgumentException("Telefone inválido");
+
+    this.telefone = telefone;
+  }
+
+  @Override
+  protected void internalSetId(Long id) {
+    this.id = id;
+  }
+}
