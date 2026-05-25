4d692ad7d2d70d04b8a85e4b93ce11a9534ef0be
Miguel Nascimento <miguel.nasto@gmail.com>
2026-05-25 19:48:12 -0300

    feat: add GlobalExceptionHandler

---
+++ api/src/main/java/com/estaciona/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.estaciona.exception;
+
+import com.estaciona.exception.domain.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+  @Value("${app.context:prod}")
+  public String appContext;
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<String> handleException(Exception ex) {
+    String message =
+        isDevContext() ? "An error occurred: " + ex.getMessage() : "Internal Error Occurred";
+    return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+  @ExceptionHandler(EntityNotFoundException.class)
+  public ResponseEntity<String> handleNotFoundEntity(EntityNotFoundException ex) {
+    return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+  }
+
+  public boolean isDevContext() {
+    return "dev".equals(appContext);
+  }
+}
