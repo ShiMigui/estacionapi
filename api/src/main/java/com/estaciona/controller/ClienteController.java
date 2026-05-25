32edf5fbe58fbbbb72eb3fd0c028cbc856c77c21
Miguel Nascimento <miguel.nasto@gmail.com>
2026-05-25 19:49:05 -0300

    feat: add Cliente controller

---
+++ api/src/main/java/com/estaciona/controller/ClienteController.java
@@ -0,0 +1,45 @@
+package com.estaciona.controller;
+
+import com.estaciona.model.Cliente;
+import com.estaciona.service.JpaClienteService;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController extends SupportController<Cliente, Long> {
+  public ClienteController(JpaClienteService service) {
+    super(service);
+  }
+
+  @GetMapping
+  public ResponseEntity<List<Cliente>> all() {
+    return super.all();
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Cliente> find(@PathVariable Long id) {
+    return super.find(id);
+  }
+
+  @PostMapping
+  public ResponseEntity<Cliente> create(@RequestBody Cliente data) {
+    return super.create(data);
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<Cliente> update(@PathVariable Long id, @RequestBody Cliente data) {
+    return super.update(id, data);
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> delete(@PathVariable Long id) {
+    return super.delete(id);
+  }
+
+  @Override
+  protected String getURL() {
+    return "/clientes";
+  }
+}
