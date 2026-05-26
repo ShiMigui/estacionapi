e5657b458e9d85bec3b34c801f241b9a7d91d8ca
Miguel Nascimento <miguel.nasto@gmail.com>
2026-05-26 13:06:17 -0300

    feat: add Carro repository

---
+++ api/src/main/java/com/estaciona/repository/CarroRepository.java
@@ -0,0 +1,20 @@
+package com.estaciona.repository;
+
+import com.estaciona.model.Carro;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CarroRepository extends JpaRepository<Carro, Long> {
+  @Override
+  @Query("SELECT DISTINCT c FROM Carro c JOIN FETCH c.modelo m JOIN FETCH m.marca")
+  List<Carro> findAll();
+
+  @Override
+  @Query("SELECT DISTINCT c FROM Carro c JOIN FETCH c.modelo m JOIN FETCH m.marca WHERE c.id = :id")
+  Optional<Carro> findById(@Param("id") Long id);
+}
