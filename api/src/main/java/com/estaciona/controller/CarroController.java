953958b663ae652639d98beb86bf9a3f24c79606
Miguel Nascimento <miguel.nasto@gmail.com>
2026-05-26 13:07:09 -0300

    feat: add Carro controller

---
+++ api/src/main/java/com/estaciona/controller/CarroController.java
@@ -0,0 +1,45 @@
+package com.estaciona.controller;
+
+import com.estaciona.model.Carro;
+import com.estaciona.service.JpaCarroService;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/carros")
+public class CarroController extends SupportController<Carro, Long> {
+  public CarroController(JpaCarroService service) {
+    super(service);
+  }
+
+  @GetMapping
+  public ResponseEntity<List<Carro>> all() {
+    return super.all();
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Carro> find(@PathVariable Long id) {
+    return super.find(id);
+  }
+
+  @PostMapping
+  public ResponseEntity<Carro> create(@RequestBody Carro data) {
+    return super.create(data);
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<Carro> update(@PathVariable Long id, @RequestBody Carro data) {
+    return super.update(id, data);
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> delete(@PathVariable Long id) {
+    return super.delete(id);
+  }
+
+  @Override
+  protected String getURL() {
+    return "/carros";
+  }
+}
