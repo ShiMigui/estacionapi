456ac6f8fbcaf17d6863865bf9603bf766eb1a42
Miguel Nascimento <miguel.nasto@gmail.com>
2026-05-28 14:09:03 -0300

    refactor: add NoBean annotation

--- api/src/main/java/com/estaciona/model/interfaces/IRepository.java
+++ api/src/main/java/com/estaciona/model/interfaces/IRepository.java
@@ -2,6 +2,8 @@ package com.estaciona.model.interfaces;
 
 import com.estaciona.model.AbstractEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
 
+@NoRepositoryBean
 public interface IRepository<Entity extends AbstractEntity<ID>, ID>
     extends JpaRepository<Entity, ID> {}
