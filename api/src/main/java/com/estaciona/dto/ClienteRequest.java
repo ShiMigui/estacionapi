da5e693404e7196399166ebca421c5379f5ce4db
Miguel Nascimento <miguel.nasto@gmail.com>
2026-05-31 13:12:41 -0300

    refactor: instance entity manually

--- api/src/main/java/com/estaciona/dto/ClienteRequest.java
+++ api/src/main/java/com/estaciona/dto/ClienteRequest.java
@@ -6,6 +6,9 @@ import com.estaciona.model.interfaces.IRequest;
 public record ClienteRequest(String nome, String telefone) implements IRequest<Cliente> {
   @Override
   public Cliente entity() {
-    return new Cliente(nome, telefone);
+    Cliente cliente = new Cliente();
+    if (nome != null) cliente.changeNome(nome);
+    if (telefone != null) cliente.changeTelefone(telefone);
+    return cliente;
   }
 }
