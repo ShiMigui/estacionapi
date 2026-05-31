37861666636b8c10f6d1c45d382bc8ba76c3d5f4
Miguel Nascimento <miguel.nasto@gmail.com>
2026-05-31 13:12:58 -0300

    refactor: instance entity manually

--- api/src/main/java/com/estaciona/dto/PrecoRequest.java
+++ api/src/main/java/com/estaciona/dto/PrecoRequest.java
@@ -6,6 +6,10 @@ import com.estaciona.model.interfaces.IRequest;
 public record PrecoRequest(String nome, Double preco, String descricao) implements IRequest<Preco> {
   @Override
   public Preco entity() {
-    return new Preco(nome, preco, descricao);
+    Preco e = new Preco();
+    if (nome != null) e.changeNome(nome);
+    if (preco != null) e.changePreco(preco);
+    if (descricao != null) e.setDescricao(descricao);
+    return e;
   }
 }
